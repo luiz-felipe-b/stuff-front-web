7ee37e3d1f0d36f768c50a647b54e137860bbb3a
Marcos Oliveira <22.10277-9@maua.br>
2025-05-31 20:09:12 -0300

    feat: Home and Dashboard v2

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Lembre de passar o endpoint no .env
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_STUFF_API || "https://stuff-back.fly.dev/"
+  baseURL: process.env.NEXT_PUBLIC_STUFF_API,
 });
 
 export default api;
